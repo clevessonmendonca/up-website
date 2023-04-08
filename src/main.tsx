16f84c77da02c036c3fbd739e78da7a84eee504a
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-04-08 09:14:15 -0300

    docs: vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Analytics } from "@vercel/analytics/react";
 
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
@@ -8,5 +9,6 @@ import { router } from "./router";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
   </React.StrictMode>
 );
