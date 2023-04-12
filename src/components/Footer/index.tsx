2544130b16513ea46f09cc5fb44cd259d20b05cf
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-04-12 11:39:10 -0300

    refactor: head metatags

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -11,17 +11,22 @@ export const Footer = () => {
           <div>
             <img src="/logotipo.webp" alt="Logo by Up Comunição Visual" />
             <div className="social-medias">
-              <a href="https://www.instagram.com/up.cvbsb/" target="_blank">
+              <a
+                href="https://www.instagram.com/up.cvbsb/"
+                target="_blank"
+                arial-label="Request quote by our Instagram"
+              >
                 <InstagramLogo />
               </a>
               <a
                 target="_blank"
                 rel="noopener noreferrer"
                 href="https://wa.me/5561999732123?text=Ol%C3%A1%2C+gostaria+de+solicitar+um+or%C3%A7amento%21"
+                arial-label="Request quote by our WhatsApp"
               >
                 <WhatsappLogo />
               </a>
-              <Link to="/">
+              <Link to="/" arial-label="Request quote by our Website">
                 <Globe />
               </Link>
             </div>
@@ -29,10 +34,27 @@ export const Footer = () => {
             <p>Todos os direitos reservados.</p>
           </div>
           <nav>
-            <Link to="/" aria-label="Go to Home Page!">Home</Link>
-            <Link to="/sobre"  aria-label="Find out More about Up Comunicação Visual.">Quem somos</Link>
-            <Link to="/servicos" aria-label="Click here to learn more about our services and products.">Serviços</Link>
-            <Link to="/contato" aria-label="Click here and get in touch with us.">Contato</Link>
+            <Link to="/" aria-label="Go to Home Page!">
+              Home
+            </Link>
+            <Link
+              to="/sobre"
+              aria-label="Find out More about Up Comunicação Visual."
+            >
+              Quem somos
+            </Link>
+            <Link
+              to="/servicos"
+              aria-label="Click here to learn more about our services and products."
+            >
+              Serviços
+            </Link>
+            <Link
+              to="/contato"
+              aria-label="Click here and get in touch with us."
+            >
+              Contato
+            </Link>
           </nav>
         </div>
         <div className="hour">
@@ -46,7 +68,11 @@ export const Footer = () => {
       <div className="clevesson-men">
         <p>
           Developed by{" "}
-          <a target="_blank" href="https://clevessoncontacto.com" aria-label="Go to developer Site">
+          <a
+            target="_blank"
+            href="https://clevessoncontacto.com"
+            aria-label="Go to developer Site"
+          >
             Aurora
           </a>
         </p>
