3464d240b1ab6fb152e28c1c2044bdcb68a1584f
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-04-12 13:57:08 -0300

    refactors

--- src/Services/Products.ts
+++ src/Services/Products.ts
@@ -1,59 +1,60 @@
 export const ProductsDb = [
   {
-    id: '1',
+    id: "1",
     title: "Adesivos",
     image: "/cards/card-adesivo.webp",
-    description: "Adesivos em geral personalizados para inovar sua marca em qualquer produto.",
+    description:
+      "Adesivos em geral personalizados para inovar sua marca em qualquer produto.",
   },
   {
-    id: '2',
+    id: "2",
     title: "Banners",
     image: "/cards/card-banner.webp",
     description:
       "Crie banners personalizados, com acabamentos da nossa equipe.",
   },
   {
-    id: '3',
+    id: "3",
     title: "Impressões UV",
     image: "/cards/card-uv.webp",
     description: "Impressões UV: em Lona, Adesivo e Tecido especial.",
-    star: true
+    star: true,
   },
   {
-    id: '4',
+    id: "4",
     title: "Cartão de Visita",
     image: "/cards/card-visit.webp",
     description: "Cartões de Visita para divulgação da sua empresa.",
   },
   {
-    id: '5',
+    id: "5",
     title: "Quadros",
     image: "/cards/card-quadro.webp",
     description: "Criação de Quadros com seu Design de preferência.",
   },
   {
-    id: '6',
+    id: "6",
     title: "Painel",
     image: "/cards/card-painel.webp",
     description: "Criamos Paineis personalizados.",
-    star: true
+    star: true,
   },
   {
-    id: '7',
+    id: "7",
     title: "Flyer ou Folder",
     image: "/cards/card-flyer.webp",
     description: "Criação de Flyers e Folders para sua empresa.",
-    star: true
+    star: true,
   },
   {
-    id: '8',
+    id: "8",
     title: "Fotos",
     image: "/cards/card-photo.webp",
     description: "Guarde momentos em um quadro de fotos.",
   },
   {
-    id: '9',
-    title: "Adesivo de Paredes",
+    id: "9",
+    title: "Papel de Paredes",
     image: "/cards/card-wall-sticker.webp",
     description: "Adesivos de Parede personalizados.",
   },
