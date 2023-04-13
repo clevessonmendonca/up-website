2fb84d5c2a35fdaa72a15459cbbe1ff2e7cd53bf
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-04-13 13:24:09 -0300

    feat: animation in business

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -140,46 +140,170 @@ export const About = () => {
           <h2>Empresas que confiam em nossos serviços</h2>
         </AnimationOnScroll>
         <div className="companies">
-          <a
-            title="Vivendas"
-            href="https://www.grupovivendas.com.br/"
-            target="_blank"
-            aria-label="Partner Company"
-          >
-            <img
-              src="/business/vivendas.webp"
-              alt="Company Supermarket Vivendas"
-            />
-          </a>
-          <a
-            title="Tavares"
-            href="https://www.instagram.com/tavaressupermercado/"
-            target="_blank"
-            aria-label="Partner Company"
-          >
-            <img
-              src="/business/tavares.webp"
-              alt="Company Tavares Supermarket"
-            />
-          </a>
-          <a title="Rei Massas Plásticas" href="" target="_blank">
-            <img
-              src="/business/rei-massas.webp"
-              alt="Company Rei Massas Plásticas"
-            />
-          </a>
-
-          <a
-            title="Autoestima"
-            href="https://www.instagram.com/dmourac_autoestima/"
-            target="_blank"
-            aria-label="Partner Company"
-          >
-            <img
-              src="/business/autoestima.webp"
-              alt="Company Estetica Autoestima"
-            />
-          </a>
+          <ul>
+            <li>
+              <a
+                title="Vivendas"
+                href="https://www.grupovivendas.com.br/"
+                target="_blank"
+                aria-label="Partner Company"
+              >
+                <img
+                  src="/business/vivendas.webp"
+                  alt="Company Supermarket Vivendas"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                title="Tavares"
+                href="https://www.instagram.com/tavaressupermercado/"
+                target="_blank"
+                aria-label="Partner Company"
+              >
+                <img
+                  src="/business/tavares.webp"
+                  alt="Company Tavares Supermarket"
+                />
+              </a>
+            </li>
+            <li>
+              <a title="Rei Massas Plásticas" href="" target="_blank">
+                <img
+                  src="/business/rei-massas.webp"
+                  alt="Company Rei Massas Plásticas"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                title="Autoestima"
+                href="https://www.instagram.com/dmourac_autoestima/"
+                target="_blank"
+                aria-label="Partner Company"
+              >
+                <img
+                  src="/business/autoestima.webp"
+                  alt="Company Estetica Autoestima"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                title="Dreams Personalizados"
+                href="#"
+                target="_blank"
+                aria-label="Partner Company"
+              >
+                <img
+                  src="/business/dreams-personalizados.webp"
+                  alt="Company Dreams Personalizados"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                title="Mundo Zoo"
+                href="#"
+                target="_blank"
+                aria-label="Partner Company"
+              >
+                <img src="/business/mundo-zoo.webp" alt="Company Mundo Zoo" />
+              </a>
+            </li>
+            <li>
+              <a
+                title="Mineirão"
+                href="#"
+                target="_blank"
+                aria-label="Partner Company"
+              >
+                <img src="/business/mineirao.webp" alt="Company Mineirão" />
+              </a>
+            </li>
+            <li>
+              <a
+                title="Nossa Kaza"
+                href="#"
+                target="_blank"
+                aria-label="Partner Company"
+              >
+                <img src="/business/nossakaza.webp" alt="Company Nosso Kaza" />
+              </a>
+            </li>
+            <li>
+              <a
+                title="The 300"
+                href="#"
+                target="_blank"
+                aria-label="Partner Company"
+              >
+                <img src="/business/the300.webp" alt="Company The 300" />
+              </a>
+            </li>
+            <li>
+              <a
+                title="Texugo's Burguers"
+                href="#"
+                target="_blank"
+                aria-label="Partner Company"
+              >
+                <img
+                  src="/business/texugos-burguer.webp"
+                  alt="Company Texugo's Burguer"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                title="Espaço 29"
+                href="#"
+                target="_blank"
+                aria-label="Partner Company"
+              >
+                <img src="/business/espaco-29.webp" alt="Company Espaço 29" />
+              </a>
+            </li>
+            <li>
+              <a
+                title="Toca do Peixe"
+                href="#"
+                target="_blank"
+                aria-label="Partner Company"
+              >
+                <img
+                  src="/business/toca-do-peixe.webp"
+                  alt="Company Toca do Peixe"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                title="Euro, Araujo, Soares & Lima"
+                href="#"
+                target="_blank"
+                aria-label="Partner Company"
+              >
+                <img
+                  src="/business/euro-araujo-soares.webp"
+                  alt="Company Euro, Araujo, Soares & Lima"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                title="Santa Maria Shopping"
+                href="#"
+                target="_blank"
+                aria-label="Partner Company"
+              >
+                <img
+                  src="/business/santa-maria-shop.webp"
+                  alt="Company Santa Maria Shopping"
+                />
+              </a>
+            </li>
+          </ul>
         </div>
       </section>
 
